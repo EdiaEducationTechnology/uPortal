c59e8a22c9e9030d8ba85b5c458e205ea333b50d
Joris Scharp <jorisscharp@gmail.com>
2014-12-17 12:47:31 +0100

    UVAUPORTAL-31: uPortal group creating based on Surf teams. The groups are imported as JSON, still needs to be converted to XML and uploaded by the import/export tool.

--- uportal-war/src/main/java/org/jasig/portal/security/mvc/LoginController.java
+++ uportal-war/src/main/java/org/jasig/portal/security/mvc/LoginController.java
@@ -20,20 +20,38 @@
 package  org.jasig.portal.security.mvc;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Enumeration;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.oltu.oauth2.client.request.OAuthClientRequest;
+import org.apache.oltu.oauth2.client.response.OAuthAuthzResponse;
+import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
+import org.apache.oltu.oauth2.common.message.types.GrantType;
+import org.jasig.portal.properties.PropertiesManager;
 import org.jasig.portal.security.IPerson;
 import org.jasig.portal.security.IPersonManager;
 import org.jasig.portal.url.IPortalUrlBuilder;
 import org.jasig.portal.url.IPortalUrlProvider;
 import org.jasig.portal.url.UrlType;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -48,7 +66,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * represent the principal and credential for each security context.
  */
 @Controller
-@RequestMapping("/Login")
 public class LoginController {
     public static final String REFERER_URL_PARAM = "refUrl";
     
@@ -56,12 +73,17 @@ public class LoginController {
     public static final String AUTH_ERROR_KEY = "up_authenticationError";
     public static final String ATTEMPTED_USERNAME_KEY = "up_attemptedUserName";
     public static final String REQUESTED_PROFILE_KEY = "profile";
+
     
     protected final Log log = LogFactory.getLog(getClass());
     protected final Log swapperLog = LogFactory.getLog("org.jasig.portal.portlets.swapper");
     
     private IPortalUrlProvider portalUrlProvider;
     private IPersonManager personManager;
+    
+    private static final String CONEXT_CLIENT_ID = PropertiesManager.getProperty("org.jasig.portal.surfconext.client.id");
+
+    private static final String CONEXT_OAUTH_KEY = PropertiesManager.getProperty("org.jasig.portal.surfconext.oauth.key");
 
     @Autowired
     public void setPersonManager(IPersonManager personManager) {
@@ -80,9 +102,10 @@ public class LoginController {
      * @param response
      * @exception ServletException
      * @exception IOException
+     * @throws OAuthSystemException 
      */
-    @RequestMapping
-    public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    @RequestMapping("/Login")
+    public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, OAuthSystemException {
         response.setHeader("Pragma", "no-cache");
         response.setHeader("Cache-Control", "no-cache");
         response.setDateHeader("Expires", 0);
@@ -140,8 +163,9 @@ public class LoginController {
             person = this.personManager.getPerson(request);
         }
 
+        boolean isPostRequest = request.getMethod().equals("POST");
         if (person == null || !person.getSecurityContext().isAuthenticated()) {
-            if (request.getMethod().equals("POST"))
+			if (isPostRequest)
                 request.getSession(false).setAttribute(AUTH_ATTEMPTED_KEY, "true");
             // Preserve the attempted username so it can be redisplayed to the user by CLogin
             String attemptedUserName = request.getParameter("userName");
@@ -149,10 +173,88 @@ public class LoginController {
                 request.getSession(false).setAttribute(ATTEMPTED_USERNAME_KEY, request.getParameter("userName"));
         }
 
-        final String encodedRedirectURL = response.encodeRedirectURL(redirectTarget);
-        response.sendRedirect(encodedRedirectURL);
+        String conextAccessToken = (String) request.getSession(false).getAttribute("conext_access_token");
+        if (person != null && !person.isGuest() && conextAccessToken == null) {
+			//Force the three-legged oauth request
+			//  http://uportal.edia.nl/edia-conext-clientwebapp/start.html
+			OAuthClientRequest authReq = OAuthClientRequest
+			   .authorizationLocation("https://api.surfconext.nl/v1/oauth2/authorize")
+			   .setClientId("https://uportal.edia.nl")
+			   .setResponseType("code")
+			   .setScope("read")
+			   .setRedirectURI("http://uportal.edia.nl/uPortal/Token")
+			   .buildQueryMessage();
+	
+			String locationUri = authReq.getLocationUri();
+			response.sendRedirect(locationUri);
+		} else {
+			final String encodedRedirectURL = response.encodeRedirectURL(redirectTarget);
+			response.sendRedirect(encodedRedirectURL);
+		}
 
     }
 
+    @RequestMapping("Token")
+	public void code(HttpServletRequest req, HttpServletResponse response) throws Exception {
+		OAuthAuthzResponse oar = OAuthAuthzResponse.oauthCodeAuthzResponse(req);
+		String code = oar.getCode();
+		
+		
+		OAuthClientRequest request = OAuthClientRequest
+                .tokenLocation("https://api.surfconext.nl/v1/oauth2/token")
+                .setGrantType(GrantType.AUTHORIZATION_CODE)
+                .setClientId(CONEXT_CLIENT_ID)
+                .setClientSecret(CONEXT_OAUTH_KEY)
+                .setRedirectURI("http://uportal.edia.nl/uPortal/Token")
+                .setCode(code)
+                .buildQueryMessage();
+		
+		String locationUri = request.getLocationUri();
+		
+
+		HttpGet httpget = new HttpGet(
+				locationUri);
+		
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		
+		CloseableHttpResponse execute = httpclient.execute(httpget);
+		
+		HttpEntity entity = execute.getEntity();
+		
+		JSONObject obj = getJSONOjectFromHttpEntity(entity);
+		
+		String access_token = obj.getString("access_token");
+		if (StringUtils.isNotEmpty(access_token)) {
+			HttpSession session = req.getSession();
+			session.setAttribute("conext_access_token", access_token);
+			
+			
+			HttpGet getGroups = new HttpGet("https://api.surfconext.nl/v1/social/rest/groups/@me");
+			getGroups.setHeader("Authorization", "Bearer " + access_token);
+			
+			
+			CloseableHttpResponse executeGetGroups = httpclient.execute(getGroups);
+			
+			HttpEntity getGroupsResponse = executeGetGroups.getEntity();
+			JSONObject jsonOjectFromHttpEntity = getJSONOjectFromHttpEntity(getGroupsResponse);
+			System.out.println(jsonOjectFromHttpEntity);
+		}
+		
+		//Hard coded for now as the request contains a different host
+		response.sendRedirect("http://localhost:8080/uPortal");
+	}
+
+	private JSONObject getJSONOjectFromHttpEntity(HttpEntity entity)
+			throws IOException, JSONException {
+		InputStream content = entity.getContent();
+		
+		StringBuilder b = new StringBuilder();
+		List<String> readLines = IOUtils.readLines(content);
+		for (String line : readLines) {
+			b.append(line);
+		}
+		JSONObject obj = new JSONObject(b.toString());
+		return obj;
+	}
 
 }
