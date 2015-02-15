b88b952493c1ac548d2e3926d32a95f863825537
Joris Scharp <jorisscharp@gmail.com>
2015-02-15 18:21:55 +0100

    Adding hard coded url, later on host needs to be configured in order to use it like this. Testing purpose commit..

--- uportal-portlets-overlay/basiclti-portlet/src/main/java/au/edu/anu/portal/portlets/basiclti/utils/LtiPortletControllerClient.java
+++ uportal-portlets-overlay/basiclti-portlet/src/main/java/au/edu/anu/portal/portlets/basiclti/utils/LtiPortletControllerClient.java
@@ -12,36 +12,36 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class LtiPortletControllerClient {
-	// HTTP GET request
-		public HashMap getRoleAndResourcelink(String groupId, String userName, String protocol, final String USER_AGENT) throws Exception {
+    // HTTP GET request
+        public HashMap getRoleAndResourcelink(String groupId, String userName, String protocol, final String USER_AGENT) throws Exception {
 
-			String url = "/api/retrieveLtiPortletLaunchParams/group/" + groupId + "/user/" + userName;
+            String url = protocol+ "://uportal.edia.nl/uPortal/api/retrieveLtiPortletLaunchParams/group/" + groupId + "/user/" + userName;
 
-			HttpClient client = new DefaultHttpClient();
-			HttpGet request = new HttpGet(url);
+            HttpClient client = new DefaultHttpClient();
+            HttpGet request = new HttpGet(url);
 
-			// add request header
-			request.addHeader("User-Agent", USER_AGENT);
+            // add request header
+            request.addHeader("User-Agent", USER_AGENT);
 
-			HttpResponse response = client.execute(request);
+            HttpResponse response = client.execute(request);
 
-			System.out.println("\nSending 'GET' request to URL : " + url);
-			System.out.println("Response Code : "
-					+ response.getStatusLine().getStatusCode());
+            System.out.println("\nSending 'GET' request to URL : " + url);
+            System.out.println("Response Code : "
+                    + response.getStatusLine().getStatusCode());
 
-			BufferedReader rd = new BufferedReader(new InputStreamReader(response
-					.getEntity().getContent()));
+            BufferedReader rd = new BufferedReader(new InputStreamReader(response
+                    .getEntity().getContent()));
 
-			StringBuffer result = new StringBuffer();
-			String line = "";
-			while ((line = rd.readLine()) != null) {
-				result.append(line);
-			}
+            StringBuffer result = new StringBuffer();
+            String line = "";
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+            }
 
-			System.out.println(result.toString());
-			HashMap<String,Object> letiResults 		= new ObjectMapper().readValue(result.toString(), HashMap.class);
-			HashMap ltiData		= (HashMap) letiResults.get("ltiLauncParameters");
-			return ltiData;
-		}
-		
+            System.out.println(result.toString());
+            HashMap<String,Object> letiResults      = new ObjectMapper().readValue(result.toString(), HashMap.class);
+            HashMap ltiData     = (HashMap) letiResults.get("ltiLauncParameters");
+            return ltiData;
+        }
+        
 }
