3c1b7f952cb9c4694010e1eef4fc7c5908568b29
Joris Scharp <jorisscharp@gmail.com>
2015-02-15 20:32:13 +0100

    TMP commit for demo, disabling SSL validation for request

--- uportal-portlets-overlay/basiclti-portlet/src/main/java/au/edu/anu/portal/portlets/basiclti/utils/LtiPortletControllerClient.java
+++ uportal-portlets-overlay/basiclti-portlet/src/main/java/au/edu/anu/portal/portlets/basiclti/utils/LtiPortletControllerClient.java
@@ -3,6 +3,7 @@ package main.java.au.edu.anu.portal.portlets.basiclti.utils;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.security.SecureRandom;
+import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.HashMap;
 
@@ -17,74 +18,73 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class LtiPortletControllerClient {
     // HTTP GET request
-        public HashMap getRoleAndResourcelink(String groupId, String userName, String protocol, final String USER_AGENT) throws Exception {
+    public HashMap getRoleAndResourcelink(String groupId, String userName, String protocol, final String USER_AGENT) throws Exception {
 
-            String url = protocol+ "://uportal.edia.nl/uPortal/api/retrieveLtiPortletLaunchParams/group/" + groupId + "/user/" + userName;
+        String url = protocol + "://uportal.edia.nl/uPortal/api/retrieveLtiPortletLaunchParams/group/" + groupId + "/user/" + userName;
 
+        HttpClient client = new DefaultHttpClient();
+        wrapClient(client);
 
-            if(StringUtils.equals("https", protocol)) {
-                disableCertificateValidationForDemo();
-            }
+        HttpGet request = new HttpGet(url);
 
-            HttpClient client = new DefaultHttpClient();
-            HttpGet request = new HttpGet(url);
-            
-            // add request header
-            request.addHeader("User-Agent", USER_AGENT);
+        // add request header
+        request.addHeader("User-Agent", USER_AGENT);
 
-            HttpResponse response = client.execute(request);
+        HttpResponse response = client.execute(request);
 
-            System.out.println("\nSending 'GET' request to URL : " + url);
-            System.out.println("Response Code : "
-                    + response.getStatusLine().getStatusCode());
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + response.getStatusLine().getStatusCode());
 
-            BufferedReader rd = new BufferedReader(new InputStreamReader(response
-                    .getEntity().getContent()));
+        BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
 
-            StringBuffer result = new StringBuffer();
-            String line = "";
-            while ((line = rd.readLine()) != null) {
-                result.append(line);
-            }
-
-            System.out.println(result.toString());
-            HashMap<String,Object> letiResults      = new ObjectMapper().readValue(result.toString(), HashMap.class);
-            HashMap ltiData     = (HashMap) letiResults.get("ltiLauncParameters");
-            return ltiData;
+        StringBuffer result = new StringBuffer();
+        String line = "";
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
         }
-        //TODO: Remove this and set key path, only for demo purpose
-        public static void disableCertificateValidationForDemo() {
-              // Create a trust manager that does not validate certificate chains
-              TrustManager[] trustAllCerts = new TrustManager[] { 
-                new X509TrustManager() {
-                    public X509Certificate[] getAcceptedIssuers() { 
-                      return new X509Certificate[0]; 
-                    }
-                    public void checkClientTrusted(X509Certificate[] certs, String authType) {}
-                    public void checkServerTrusted(X509Certificate[] certs, String authType) {}
-                }};
 
-                // Ignore differences between given hostname and certificate hostname
-                HostnameVerifier hv = new HostnameVerifier() {
+        System.out.println(result.toString());
+        HashMap<String, Object> letiResults = new ObjectMapper().readValue(result.toString(), HashMap.class);
+        HashMap ltiData = (HashMap) letiResults.get("ltiLauncParameters");
+        return ltiData;
+    }
 
-                @Override
-                public boolean verify(String arg0, SSLSession arg1) {
-                    return true;
+    // TODO: Remove this and set key path, only for demo purpose
+    public HttpClient wrapClient(HttpClient base) {
+        try {
+            SSLContext ctx = SSLContext.getInstance("TLS");
+            X509TrustManager tm = new X509TrustManager() {
+
+                public void checkClientTrusted(X509Certificate[] xcs, String string) throws CertificateException {
                 }
-                };
 
-                // Install the all-trusting trust manager
-                try {
-                  SSLContext sc = SSLContext.getInstance("SSL");
-                  sc.init(null, trustAllCerts, new SecureRandom());
-                  HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-                  HttpsURLConnection.setDefaultHostnameVerifier(hv);
-              } catch (Exception e) {}
+                public void checkServerTrusted(X509Certificate[] xcs, String string) throws CertificateException {
+                }
+
+                public X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+            };
+            ctx.init(null, new TrustManager[] { tm }, null);
+            SSLSocketFactory ssf = new SSLSocketFactory(ctx);
+            ssf.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+            ClientConnectionManager ccm = base.getConnectionManager();
+            SchemeRegistry sr = ccm.getSchemeRegistry();
+            sr.register(new Scheme("https", ssf, 443));
+            return new DefaultHttpClient(ccm, base.getParams());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
         }
+    }
 }
